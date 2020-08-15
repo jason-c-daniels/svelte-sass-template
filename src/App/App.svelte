99f3c6c4b553c876f25b963f9f29b6bc27b47c13
Jason Daniels <jason.c.daniels@gmail.com>
2020-08-15 12:32:22 -0500

    Initial commit

---
+++ src/App/App.svelte
@@ -0,0 +1,21 @@
+<script>
+    import GlobalCss from "../GlobalCss";
+    import Credits from "../components/Credits";
+
+    export let name;
+    export let appSettings = {applicationName: "WARNING: Please pass appSettings from within main.js props."};
+
+</script>
+<style>
+    @import "App.css";
+</style>
+
+<svelte:head>
+    <title>{appSettings.applicationName}</title>
+</svelte:head>
+
+<GlobalCss/>
+
+<main>
+    <Credits/>
+</main>
---
+++ src/components/_ComponentTemplate/Component.svelte
@@ -0,0 +1,14 @@
+<style>
+    @import "Component.css";
+</style>
+
+<script>
+    <!-- TODO: put custom code here -->
+</script>
+
+<!--
+    TODO: Put component markup here.
+
+    TODO: Rename Component.svelte and Component.css (and all the index.js reference)
+          to the match containing folder name. (This will help you keep track of the component you're working on)
+-->
